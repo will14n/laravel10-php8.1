c3ed9a6edb2d5501e0d91c00d2afe7187777ddee
Willian <willianf4m@gmail.com>
2023-09-21 18:32:55 -0300

    feat: created support controller, model and migration

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
     public function index() {
-        return view('admin/supports/index');
+        $support = Support::all();
+        
+        return view('admin/supports/index', compact('support'));
     }
 }
