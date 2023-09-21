f4846f41599c951fd45ed505a45c52fc5fd15728
Willian <willianf4m@gmail.com>
2023-09-21 18:14:03 -0300

    feat: created routes contact and support with her controllers and views

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Listagem dos Suports</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/support', [SupportController::class, 'index'])->name(('supports.index'));
+
+Route::get('/contact', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
