445491a2adb44f0ef48c03d887b8e893492e1c54
Willian <willianf4m@gmail.com>
2023-09-25 12:36:10 -0300

    feat: criado model e migration do módulo Suporte

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -8,9 +8,59 @@ use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index() {
-        $support = Support::all();
+    public function index(Support $support) {
+        $supports = $support->all();
+
+        return view('admin/supports/index', compact('supports'));
+    }
+
+    public function show( string|int $id) {
+        if(!$support = Support::find($id))
+            return back();
+        return view('admin/supports/show', compact('support'));
+    }
+
+    public function create() {
+        return view('admin/supports/create');
+    }
+
+    public function store(Request $request, Support $support) {
+        // return view('admin/supports/create');
+        $data = $request->all();
+        $data['status'] = 'a';
+
+        $support->create($data);
+        // dd($request->only(['subject', 'body']));
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(Support $support, string|int $id) {
+        // return view('admin/supports/create');
+        if(!$support = $support->find($id)) 
+            return back();
         
-        return view('admin/supports/index', compact('support'));
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string|int $id) {
+        // return view('admin/supports/create');
+        if(!$support = $support->find($id)) 
+            return back();
+        
+        $support->update($request->only([
+            'subject',
+            'description'
+        ]));
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(Support $support, string|int $id) {
+        // return view('admin/supports/create');
+        if(!$support = $support->find($id)) 
+            return back();
+        
+        $support->delete();
+
+        return redirect()->route('supports.index');
     }
 }
