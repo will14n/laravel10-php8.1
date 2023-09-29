ccee9f82062948c04e1183592a6f01b6367cd4de
Willian <willianf4m@gmail.com>
2023-09-29 14:30:43 -0300

    feat: acrescentado na view demais recursos crud do módulo

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,9 +1,15 @@
 <h1>Nova Dúvida</h1>
 
+@if($errors->any())
+    @foreach ($errors->all() as $error )
+        {{ $error }}
+    @endforeach
+@endif
+
 <form action="{{ route('supports.store') }}" method="POST">
     <!-- <input type="hidden" value="{{ csrf_token() }}" name="_token" /> -->
     @csrf
-    <input type="text" placeholder="Assunto" name="subject" />
-    <textarea name="description" cols="30" rows="5" placeholder="Descrição"></textarea>
+    <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}" />
+    <textarea name="description" cols="30" rows="5" placeholder="Descrição">{{ old('description') }}</textarea>
     <button type="submit">Enviar</button>   
 </form>
